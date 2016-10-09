f6f9d8f654826cb16aab8aaac16fd7a7c38e6b8b
mi.rufeng <958936985@qq.com>
2016-10-09 17:40:38 +0800

    update baseActivity

--- app/src/main/java/com/bright/cloudcompatlib/base/BaseActivity.java
+++ app/src/main/java/com/bright/cloudcompatlib/base/BaseActivity.java
@@ -1,20 +1,27 @@
 package com.bright.cloudcompatlib.base;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -24,6 +31,9 @@ import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import com.bright.cloudcompatlib.R;
+import com.bright.cloudcompatlib.core.SystemBarTintManager;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by tci_mi on 16/10/8.
@@ -34,11 +44,14 @@ public abstract class BaseActivity extends AppCompatActivity {
     public boolean isLandscape = false;
     protected BaseActivity mBaseActivity = null;
     protected BaseApplication mApplication = null;
+
     protected boolean mIsCancelAction = false;
     protected boolean mIsLogoutAction = false;
+
     Toast mToast = null;
     private long waitTime = 2000;
     private long touchTime = 0;
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(@NonNull Bundle savedInstanceState) {
@@ -47,6 +60,8 @@ public abstract class BaseActivity extends AppCompatActivity {
         mApplication = (BaseApplication) this.getApplication();
         mApplication.add(this);
         mBaseActivity = this;
+
+        initSystemBarTint();
         SupportDisplay.initLayoutSetParams(BaseActivity.this);
     }
 
@@ -81,14 +96,32 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     public void setContentView(View view, ViewGroup.LayoutParams params) {
         super.setContentView(view, params);
+        ButterKnife.bind(this);
         resetLayout();
     }
 
     @Override
-    public void setContentView(int layoutResID) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        super.setContentView(layoutResID);
+    public void setContentView(View view) {
+        super.setContentView(view);
+        ButterKnife.bind(this);
         resetLayout();
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+        ButterKnife.bind(this);
+        resetLayout();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:// 点击返回图标事件
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void baseStartActivity(Context packageContext, Class<?> cls) {
@@ -129,6 +162,11 @@ public abstract class BaseActivity extends AppCompatActivity {
             return false;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends View> T findView(int id) {
+        return (T) findViewById(id);
     }
 
     /**
@@ -233,6 +271,8 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     }
 
+    // ************　ToolBar 相关　start *************** //
+
     /**
      * 公用提示框
      */
@@ -254,7 +294,85 @@ public abstract class BaseActivity extends AppCompatActivity {
         });
     }
 
-    // ************　共通组件相关　*************** //
+    /**
+     * 子类可以重写改变状态栏颜色
+     */
+    protected int setStatusBarColor() {
+        return getColorPrimary();
+    }
+
+    /**
+     * 子类可以重写决定是否使用透明状态栏
+     */
+    protected boolean translucentStatusBar() {
+        return false;
+    }
+
+    /**
+     * 设置状态栏颜色
+     */
+    protected void initSystemBarTint() {
+        Window window = getWindow();
+        if (translucentStatusBar()) {
+            // 设置状态栏全透明
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(Color.TRANSPARENT);
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            }
+            return;
+        }
+        // 沉浸式状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //5.0以上使用原生方法
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(setStatusBarColor());
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //4.4-5.0使用三方工具类
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setStatusBarTintColor(setStatusBarColor());
+        }
+    }
+
+    /**
+     * 获取主题色
+     */
+    public int getColorPrimary() {
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        return typedValue.data;
+    }
+
+    /**
+     * 获取深主题色
+     */
+    public int getDarkColorPrimary() {
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValue, true);
+        return typedValue.data;
+    }
+
+    /**
+     * 初始化 Toolbar
+     */
+    public void initToolBar(Toolbar toolbar, boolean homeAsUpEnabled, String title) {
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(homeAsUpEnabled);
+    }
+    // ************　ToolBar相关　end *************** //
+
+    public void initToolBar(Toolbar toolbar, boolean homeAsUpEnabled, int resTitle) {
+        initToolBar(toolbar, homeAsUpEnabled, getString(resTitle));
+    }
+
+    // ************　对话框相关　start *************** //
     private android.support.v7.app.AlertDialog showDialog(Context context, String title, String message, View contentView,
                                                           String positiveBtnText, String negativeBtnText,
                                                           DialogInterface.OnClickListener positiveCallback,
@@ -293,13 +411,20 @@ public abstract class BaseActivity extends AppCompatActivity {
         return showDialog(context, title, message, null, positiveBtnText, negativeBtnText, positiveCallback, negativeCallback, cancelable);
     }
 
-    public interface PressOKButtonListener {
-        void onOKButtonPressed();
+    public void showLoading() {
+        if (dialog != null && dialog.isShowing()) return;
+        dialog = new ProgressDialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setMessage("请求网络中...");
+        dialog.show();
     }
 
-
-    public interface PressCancelButtonListener {
-        void onCancelButtonPressed();
+    public void dismissLoading() {
+        if (dialog != null && dialog.isShowing()) {
+            dialog.dismiss();
+        }
     }
 
     /**
@@ -312,5 +437,5 @@ public abstract class BaseActivity extends AppCompatActivity {
             backgroundAlpha(1f);
         }
     }
-
+    // ************　对话框相关　end *************** //
 }
